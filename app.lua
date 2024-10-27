2410cbf407fda6c86f21ab4fd78a73dd2ff40580
Vu Tran <vutt1@hasaki.vn>
2024-10-27 18:50:46 +0700

    update

--- app.lua
+++ app.lua
@@ -1,8 +1,36 @@
 local lapis = require("lapis")
+local json = require("cjson")
 local app = lapis.Application()
+app:enable("etlua")
+app.layout = require("views.index")
+local flatdb = require("flatjsondb")
+  local db = flatdb('./db')
+app:get("/services", function(self)
+  self.current_view = "views.services"
+  return { render = "index"}
+end)
+app:get("/team", function(self)
+  self.current_view = "views.team"
+  return { render = "index"}
+end)
+app:get("/faq", function(self)
+  self.current_view = "views.faq"
+  return { render = "index"}
+end)
+app:get("/", function(self)
 
-app:get("/", function()
-  return "Welcome to Lapis " .. require("lapis.version")
+  -- if not db.menu then
+  --   db.menu = {}
+  -- end
+  -- self.menu = db.menu.data
+  -- ngx.log(ngx.ERR, json.encode(self.menu))
+  -- return db.menu
+  -- local val = db.page.key
+  -- db.page.key = 'value'
+  -- db:save()
+  self.current_view = "views.home"
+  -- return "Welcome to Lapis " .. require("lapis.version") .. " value:" .. val 
+  return { render = "index"}
 end)
 
 return app
