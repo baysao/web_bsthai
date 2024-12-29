d150f800c37213e765cd16cb57c990d97992ee15
Vu Tran <vutt1@hasaki.vn>
2024-12-29 13:17:24 +0700

    add gallery

--- app.lua
+++ app.lua
@@ -4,43 +4,76 @@ local app = lapis.Application()
 app:enable("etlua")
 app.layout = require("views.index")
 local flatdb = require("flatjsondb")
-  local db = flatdb('./db')
-app:get("/services", function(self)
-  self.current_view = "views.services"
-  return { render = "index"}
-end)
-app:get("/team", function(self)
-  self.current_view = "views.team"
-  self.members = db["member.json"]
-  return { render = "index"}
-end)
+local db = flatdb("./db")
+app:get(
+    "/services",
+    function(self)
+        self.current_view = "views.services"
+        return {render = "index"}
+    end
+)
+app:get(
+    "/team",
+    function(self)
+        self.current_view = "views.team"
+        local _m = db["member.json"]
+        local _t = {}
+        for k, v in pairs(_m) do
+            _t[#_t + 1] = v;
+        end
 
-app:get("/member/:member", function(self)
-  
-  self.current_view = "views.member"
-  -- ngx.log(ngx.ERR, self.params.member)
-  self.member = db["member.json"][self.params.member]
-  -- ngx.log(ngx.ERR, json.encode(self.member))
-  return { render = "index"}
-end)
-app:get("/faq", function(self)
-  self.current_view = "views.faq"
-  return { render = "index"}
-end)
-app:get("/", function(self)
+        -- ngx.log(ngx.ERR, json.encode(self.members))
+        table.sort(_t, function(k1,k2) return k1.index < k2.index end)
+        self.members = _t
+        -- ngx.log(ngx.ERR, json.encode(t))
 
-  -- if not db.menu then
-  --   db.menu = {}
-  -- end
-  -- self.menu = db.menu.data
-  -- ngx.log(ngx.ERR, json.encode(self.menu))
-  -- return db.menu
-  -- local val = db.page.key
-  -- db.page.key = 'value'
-  -- db:save()
-  self.current_view = "views.home"
-  -- return "Welcome to Lapis " .. require("lapis.version") .. " value:" .. val 
-  return { render = "index"}
-end)
+        return {render = "index"}
+    end
+)
+
+app:get(
+    "/member/:member",
+    function(self)
+        self.current_view = "views.member"
+        -- ngx.log(ngx.ERR, self.params.member)
+        self.member = db["member.json"][self.params.member]
+       
+        
+        return {render = "index"}
+    end
+)
+app:get(
+    "/faq",
+    function(self)
+        self.current_view = "views.faq"
+        return {render = "index"}
+    end
+)
+app:get(
+    "/gallery",
+    function(self)
+        self.gallery_list = db["gallery.json"]
+        ngx.log(ngx.ERR, json.encode(self.gallery_list))
+        self.current_view = "views.gallery"
+        return {render = "index"}
+    end
+)
+app:get(
+    "/",
+    function(self)
+        -- if not db.menu then
+        --   db.menu = {}
+        -- end
+        -- self.menu = db.menu.data
+        -- ngx.log(ngx.ERR, json.encode(self.menu))
+        -- return db.menu
+        -- local val = db.page.key
+        -- db.page.key = 'value'
+        -- db:save()
+        self.current_view = "views.home"
+        -- return "Welcome to Lapis " .. require("lapis.version") .. " value:" .. val
+        return {render = "index"}
+    end
+)
 
 return app
